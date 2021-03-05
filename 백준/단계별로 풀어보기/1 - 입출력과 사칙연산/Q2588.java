35015b03c8bba63cb24ec6f2bcbfde7858cd1b42
minuk <40295076+minuk1236@users.noreply.github.com>
2021-03-05 18:00:19 +0900

    입출력과 사칙연산

---
+++ 백준/단계별로 풀어보기/1 - 입출력과 사칙연산/Q2588.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Q2588 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+
+		System.out.println(num1 * (num2 % 10));
+		System.out.println(num1 * ((num2 % 100) / 10));
+		System.out.println(num1 * (num2 / 100));
+		System.out.println(num1 * num2);
+
+	}
+
+}
