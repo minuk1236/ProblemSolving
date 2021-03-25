e6182348944852fcdd48a2c203742c09ee6f2c2a
minuk <40295076+minuk1236@users.noreply.github.com>
2021-03-25 17:49:58 +0900

    수학

---
+++ 백준/수학/Q1676.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Q1676 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		int count = 0;
+
+		while (n >= 5) {
+			count += (n / 5);
+			n /= 5;
+		}
+
+		System.out.println(count);
+	}
+
+}
